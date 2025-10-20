33339d6101c6c86c42e9c92df50717933365f155
xucongyong <ironvanxv@gmail.com>
2025-10-20 18:11:35 +0800

    update

--- electron/main.ts
+++ electron/main.ts
@@ -31,7 +31,7 @@ function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
-  })
+  });
 
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
@@ -46,9 +46,7 @@ function createWindow() {
   }
 }
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// explicitly with Cmd + Q
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
